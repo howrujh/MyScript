5b89f4cdbe0facf4a9251f4ecd14a55aa1dc7d46
jinhwan <howrujh@gmail.com>
2016-10-14 19:34:04 +0900

    * cscope support

--- mk_cch_tag.bat
+++ mk_cch_tag.bat
@@ -4,5 +4,5 @@ set _target="\.c \.h"
 set _bann="~ # .o .cmd .bak .hwc .hwl .txt"
 
 
-dir "c:\Program Files (x86)\Freescale\CWS12v5.1\lib\hc12c" %cd% /s/b | findstr %_target% | findstr /v %_bann% > tag_list.txt
-ctags -e -L tag_list.txt
+dir %cd% "c:\dev\freescale" /s/b | findstr %_target% | findstr /v %_bann% > cscope.files
+rem ctags -e -L tag_list.txt
